9c21d6d93f9b44df247a23126f522ebe8751cbba
Dylan <pl2023379@365.stucom.com>
2024-05-27 13:10:26 +0200

    Features(updateEventAJAX) : update with AJAX done
    -  Now we can update our events by AJAX.

---
+++ src/assets/js/updateEventAJAX.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+
+    $("#AjaxUpdateEventButton").click(function () {
+        // Hacemos la solicitud ajax al servidor 'email_validator.php'.
+        let oldEventNameAJAX = $("select[name='eventName']").val();
+        let newEventNameAJAX = $("input[name='newEventName']").val();
+        let newEventLocationAJAX = $("input[name='newEventLocation']").val();
+        let newEventTypeAJAX = $("input[name='newEventType']").val();
+
+        $("#newEventNameError").text("");
+
+        if (oldEventNameAJAX === '' || newEventNameAJAX === '' || newEventLocationAJAX === '' || newEventTypeAJAX === '') {
+            $("#newEventNameError").text("Todos los campos son obligatorios.").css("color", "red");
+            return;
+        }
+
+        $.ajax({
+            url: "../controller/updateEventAJAX.php",
+            type: "POST",
+            dataType: "json",
+            data: {
+                oldEventName: oldEventNameAJAX,
+                newEventName: newEventNameAJAX,
+                newEventLocation: newEventLocationAJAX,
+                newEventType: newEventTypeAJAX
+            },
+            success: function (data) {
+
+                if (data.message) {
+
+                    window.location.href = "../view/admin_profile.php";
+                }
+
+            },
+            error: function () {
+                console.log("Error al procesar la solicitud.");
+
+            }
+        });
+    });
+});
---
+++ src/controller/UpdateEventAJAX.php
@@ -0,0 +1,44 @@
+<?php
+require_once 'Database.php';
+session_start();
+
+$oldEventName = isset($_POST['oldEventName']) ? ($_POST['oldEventName']) : '';
+$newEventName = isset($_POST['newEventName']) ? ($_POST['newEventName']) : '';
+$newEventLocation = isset($_POST['newEventLocation']) ? ($_POST['newEventLocation']) : '';
+$newEventType = isset($_POST['newEventType']) ? ($_POST['newEventType']) : '';
+
+// Validación desde el servidor para que los campos no esten vacios.
+if (empty($oldEventName) || empty($newEventName) || empty($newEventLocation) || empty($newEventType)) {
+    $response['status'] = 'error';
+    $response['message'] = "Todos los campos son obligatorios.";
+    echo json_encode($response);
+    exit();
+}
+
+try {
+    $db = new Database();
+    $conn = $db->getConnection();
+
+    $stmt = $conn->prepare("UPDATE EVENTO 
+    SET nombre = :newEventName, localizacion = :newEventLocation, tipo = :newEventType 
+    WHERE nombre = :oldEventName");
+    $stmt->bindParam(':oldEventName', $oldEventName, PDO::PARAM_STR);
+    $stmt->bindParam(':newEventName', $newEventName, PDO::PARAM_STR);
+    $stmt->bindParam(':newEventLocation', $newEventLocation, PDO::PARAM_STR);
+    $stmt->bindParam(':newEventType', $newEventType, PDO::PARAM_STR);
+
+    if ($stmt->execute()) {
+        $responde['status'] = 'success';
+        $response['message'] = "El evento $oldEventName se ha actualizado correctamente mediante AJAX.";
+        $_SESSION['success_message'] = $response['message'];
+    } else {
+        $responde['status'] = 'error';
+        $response['message'] = "Error al actualizar el evento mediante AJAX.";
+        $_SESSION['success_message'] = $response['message'];
+    }
+} catch (PDOException $updateError) {
+    $response['status'] = 'error';
+    $response['message'] = "Error de conexión a la base de datos: " . $updateError->getMessage();
+}
+
+echo json_encode($response);
